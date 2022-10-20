a3d5c8123d2e194989aa920bd05096f6106158eb
Pedro Pimentel <pedroamazonet@gmail.com>
2022-10-20 11:15:12 -0300

    Módulo usuário

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:3000/os'
+const apiUrl = 'https://jsonplaceholder.typicode.com'
 
 const api = axios.create({
     baseURL: apiUrl
---
+++ src/pages/Users.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {useEffect, useState} from "react";
+import api from "../api/api";
+
+import {
+    BrowserRouter as Router,
+    // Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+export default function Users(){
+    const [users, setUsers] = useState<any[]>([]);
+    const getUsers = async () => {
+        await api.get("/users").then((response) => setUsers(response.data));
+    };
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    return(
+        <div className="App">
+            <h2>Listagem de usuários</h2>
+            <ul>
+                {users &&
+                    users.map((user) =>(
+                        <li key={user.id}>
+                            <Link to={`${user.id}/posts`}>{user.name}</Link>
+                            <br />
+                            <br />
+                        </li>
+                    ))
+                } 
+            </ul>
+        </div>
+    );
+
+}
